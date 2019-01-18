b235b5d607db1814eaec6147fb5cbdbb5c0f5dde
axecopfire <braun.sn11@gmail.com>
2019-01-18 10:35:16 -0500

    logo shadow tshooting continued

--- docs/js/main.js
+++ docs/js/main.js
@@ -105,9 +105,7 @@ window.onload = function () {
   var logo = document.querySelector(".logo");
 
   logo.addEventListener("mousemove", function(e) {
-    logo.style.filter = "drop-shadow(" + Math.round((e.screenX - e.clientX) / 5) + "px " + Math.round((e.screenY - e.clientY) / 5) + "px 5px rgba(0, 0, 0, 0.5))";
-    console.log(e.clientX, e.clientY);
-    logo.style.filter = "-webkit-drop-shadow(" + Math.round((e.screenX - e.clientX) / 5) + "px " + Math.round((e.screenY - e.clientY) / 5) + "px 5px rgba(0, 0, 0, 0.5))";
+    logo.style.filter = "drop-shadow(" + Math.round((e.screenX - e.clientX) / 5) + "px " + Math.round((e.screenY - e.clientY) / 5) + "px 5px rgba(0, 0, 0, 0.5))" + "-webkit-drop-shadow(" + Math.round((e.screenX - e.clientX) / 5) + "px " + Math.round((e.screenY - e.clientY) / 5) + "px 5px rgba(0, 0, 0, 0.5))";
     console.log(e.clientX, e.clientY);
   });
 }
