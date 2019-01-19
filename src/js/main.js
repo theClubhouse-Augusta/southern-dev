0a91aa20fb5398aad5fc891e4bf6d9e279e21f72
axecopfire <braun.sn11@gmail.com>
2019-01-19 11:09:58 -0500

    There was a glitch in the matrix shadow I think is fixed now

--- src/js/main.js
+++ src/js/main.js
@@ -105,13 +105,13 @@ window.onload = function () {
   var logo = document.querySelector(".logo");
 
   logo.addEventListener("mousemove", function(e) {
-    var xPosition = e.screenX;
-      xPosition -= e.clientX;
-    var yPosition = e.screenY;
-      yPosition -= e.clientY;
+    var xPosition = e.clientX;
+        xPosition = Math.round(- xPosition / 50);
+    var yPosition = e.clientY;
+        yPosition = Math.round(yPosition / 50);
 
-    var dropShadow = "drop-shadow(" + xPosition  + "px " + yPosition  + "px 5px rgba(0, 0, 0, 0.5));";
-
+    var dropShadow = "drop-shadow(" + xPosition  + "px " + yPosition  + "px 5px rgba(0, 0, 0, 0.5))";
+    // console.log(xPosition, yPosition);
     logo.style.filter = dropShadow;
   });
 }
