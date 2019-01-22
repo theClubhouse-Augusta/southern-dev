aa8773cc80044c9fda610e8dc5dbdfa6af8f9bd7
axecopfire <sbraun990@gmail.com>
2019-01-21 19:44:49 -0500

    babel packages added

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,20 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
+// var cache = require('gulp-cache');
 
+// JS Gulp Plugins
+var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+
+gulp.task('html', function() {
+  return gulp.src('src/*.html')
+    .pipe(gulp.dest('docs'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
 
   gulp.task('sass', function(){
     return gulp.src('src/sass/*.sass')
@@ -15,11 +27,17 @@ var cache = require('gulp-cache');
   });
 
   gulp.task('js', function() {
-    return gulp.src('src/js/*.js')
-    .pipe(gulp.dest('docs/js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp.src('src/js/**/*.js')
+      .pipe(sourcemaps.init())
+      .pipe(babel({
+        presets: ['@babel/env']
+      }))
+      .pipe(concat('all.js'))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('docs/js'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
   });
 
   gulp.task('images', function() {
@@ -28,6 +46,9 @@ var cache = require('gulp-cache');
         interlaced: true
       }))
       .pipe(gulp.dest('docs/images'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
   });
 
   gulp.task('browserSync', function() {
@@ -41,6 +62,8 @@ var cache = require('gulp-cache');
   gulp.task('watch', ['sass', 'js', 'browserSync'], function() {
     gulp.watch('src/sass/*.sass', ['sass']);
     gulp.watch('src/js/*.js', ['js']);
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/images/*.+(png|jpg|gif|svg)', ['images']);
   });
 
 
