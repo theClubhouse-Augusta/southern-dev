ec06e96ef9af669764e0472b0709af499cebf5bf
axecopfire <sbraun990@gmail.com>
2019-01-23 14:06:38 -0500

    changed some wording

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 window.onload = function () {
 
   /*!
@@ -7,7 +9,7 @@ window.onload = function () {
    * @return {String}      The serialized form data
    */
 
-  const serialize = function (form) {
+  var serialize = function serialize(form) {
 
     // Setup our serialized data
     var serialized = {};
@@ -29,54 +31,52 @@ window.onload = function () {
       }
 
       // Convert field data to a query string
-      else if ((field.type !== 'checkbox' && field.type !== 'radio') || field.checked) {
-        serialized[encodeURIComponent(field.name)] = encodeURIComponent(field.value);
-      }
+      else if (field.type !== 'checkbox' && field.type !== 'radio' || field.checked) {
+          serialized[encodeURIComponent(field.name)] = encodeURIComponent(field.value);
+        }
     }
     return serialized;
   };
 
   /* Subscribe Form submittal */
-/*  var form = document.forms.namedItem('emailAdd');
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-
-    var formData = serialize(form);
-    fetch("https://gothic-welder-217319.appspot.com", {
-        method: "POST",
-        headers: {
-          "content-type": "text/plain"
-        },
-        body: JSON.stringify(formData)
-      })
-      .then(function (resp) {
-        return resp.text();
-      })
-      .then(function (data) {
-        console.log(data);
-        results.textContent = data;
-      })
-      .catch(function (error) {
-        console.error(error);
-      })
-  });
-*/
-
+  /*  var form = document.forms.namedItem('emailAdd');
+    form.addEventListener('submit', function (e) {
+      e.preventDefault();
+  
+      var formData = serialize(form);
+      fetch("https://gothic-welder-217319.appspot.com", {
+          method: "POST",
+          headers: {
+            "content-type": "text/plain"
+          },
+          body: JSON.stringify(formData)
+        })
+        .then(function (resp) {
+          return resp.text();
+        })
+        .then(function (data) {
+          console.log(data);
+          results.textContent = data;
+        })
+        .catch(function (error) {
+          console.error(error);
+        })
+    });
+  */
 
   /*
   1. Button that enables instagram filters
   2. It is hidden at the top left corner of the splash page
   */
   var instaFilters = document.querySelector(".insta-filters"),
-   splashImg = document.getElementById("splash-img"),
-   devBtn = document.getElementById("dev-btn");
+      splashImg = document.getElementById("splash-img"),
+      devBtn = document.getElementById("dev-btn");
 
-
-  function GenerateFilters (){
+  function GenerateFilters() {
     var filterArray = ["sepia", "default"];
 
     // 1. Create insta filters
-    filterArray.forEach(function(element) {
+    filterArray.forEach(function (element) {
       var filterButton = document.createElement("button");
       filterButton.id = element;
       filterButton.innerHTML = element;
@@ -96,24 +96,29 @@ window.onload = function () {
           splashImg.style.filter = "";
           break;
       };
-
     });
   }
 
   devBtn.addEventListener("click", GenerateFilters);
 
-
   /* Logo Shadow Hover animation */
   var logo = document.querySelector(".logo");
 
-  logo.addEventListener("mousemove", function(e) {
+  logo.addEventListener("mousemove", function (e) {
     var xPosition = e.clientX;
-        xPosition = Math.round(- xPosition / 50);
+    xPosition = Math.round(-xPosition / 50);
     var yPosition = e.clientY;
-        yPosition = Math.round(yPosition / 50);
+    yPosition = Math.round(yPosition / 50);
 
-    var dropShadow = "drop-shadow(" + xPosition  + "px " + yPosition  + "px 5px rgba(0, 0, 0, 0.5))";
+    var dropShadow = "drop-shadow(" + xPosition + "px " + yPosition + "px 5px rgba(0, 0, 0, 0.5))";
     // console.log(xPosition, yPosition);
     logo.style.filter = dropShadow;
   });
-}
+
+  /* Babel Test */
+  var cLog = function cLog() {
+    console.log("hello world");
+  };
+  cLog();
+};
+//# sourceMappingURL=main.js.map
