b4d61415766411228e61a12e9d11a8b4d4fb963a
axecopfire <sbraun990@gmail.com>
2019-01-24 11:45:02 -0500

    speaker json updated and some styles

--- docs/js/main.js
+++ docs/js/main.js
@@ -158,18 +158,24 @@ window.onload = function () {
 
 var speakers = [{
   "name": "Jerome Hardaway",
+  "title": "Geek &amp; Arms of Vets Who Code",
   "bio-short": "Front End Developer && Geek &amp; Arms of Vets Who Code. After serving the Air Force as a member of Security Forces, he transitioned into code during the Great Recession with his first job working on Databases with the Dept of Homeland Security.",
+  "bio-long": "Jerome Hardaway is a Front End Developer && Geek &amp; Arms of Vets Who Code. After serving the Air Force as a member of Security Forces, he transitioned into code during the Great Recession with his first job working on Databases with the Dept of Homeland Security.\n    While building and leading engineers at Vets Who Code, his main duties is building relationships, telling the stories of our veterans, talks, learning and passing higher level training to the incoming leadership team as they support and grow the next groups of entry-level && junior developers.",
   "image": "jerome.jpg"
 }, {
   "name": "Eric Harrison",
+  "title": "Co-founder and CTO of Moonrise",
   "bio-short": "co-founder and CTO of Moonrise, a social-impact startup trying to use AI to connect financially challenged workers to awesome jobs. He's the author of An Introduction to Robot Programming and has spent his career trying to automate-away the worst parts of life.",
   "image": "eric.png"
 }, {
   "name": "Harsha Vemulapalli",
+  "title": "Head of Design for Robert Bosch's North Amerian Region",
   "bio-short": "Head of Design for Robert Bosch’s North American region. In this capacity, he leads UX efforts focused on building on Bosch’s history of excellence as a leading global supplier of technology and services by driving adoption of innovative, design-thinking solutions and services. His teams span the gamut from Industrial Design, Service Design, Interaction Design, Visual Design and User Research.",
+  "bio-long": "Harsha Vemulapalli is the Head of Design for Robert Bosch’s North American region. In this capacity, he leads UX efforts focused on building on Bosch’s history of excellence as a leading global supplier of technology and services by driving adoption of innovative, design-thinking solutions and services. His teams span the gamut from Industrial Design, Service Design, Interaction Design, Visual Design and User Research.\nPrior to joining Bosch, Harsha served in UX leadership roles with the United States Air Force and the Centers for Disease Control and Prevention, and worked on design and product teams at Blackbaud, Morris DigitalWorks, Mirant and Softsys (hey it was the Tech Bubble days and all the good names were taken).\nLike the Ultimate Warrior, Harsha hails from parts unknown. He would also prefer to be introduced as “weight unknown” and \“height unknown.\”",
   "image": "Harsha.jpg"
 }, {
   "name": "Mark Noonan",
+  "title": "Web Developer at Content Thread",
   "bio-short": "Web developer at Content Thread (a digital agency and consulting firm specializing in AEM) and a member of Code for Atlanta's leadership team. He also serves as a program developer for People Making Progress, an Atlanta-based nonprofit serving adults with developmental disabilities at home, work, and in the community. In 2017 he led the Paratransit Pal project, which won AT&T's Civic Coding Challenge, after which the project team donated the entire $40,000 prize to charity.",
   "image": "mark.jpg"
 }];
