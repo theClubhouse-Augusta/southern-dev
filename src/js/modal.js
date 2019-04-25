3eb693e6d5dc0398812d20171c0bbdfbe38ac58f
RudsonA <rudson.augustin@yahoo.com>
2019-04-25 11:30:45 -0400

    bootsrap, added padding to div to allow button  to be clicked

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,21 +5,21 @@ let modal = document.getElementsByClassName('myModal');
 let modalTrigger = document.getElementsByClassName("modalTrigger");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close");
 
 // When the user clicks the button, open the modal 
-// modalTrigger.onclick = function() {
-//   modal.style.display = "block";
-// }
+modalTrigger.onclick = function() {
+  modal.style.display = "block";
+}
 
-modalTrigger.addEventListener("click", ()=>{
-    modal.style.display = "block";
-})
+// modalTrigger.addEventListener("click", ()=>{
+//     modal.style.display = "block";
+// })
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
