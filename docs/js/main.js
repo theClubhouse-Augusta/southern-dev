f00a72f6ea843ab859ead3e815b9631708b34b8a
RudsonA <rudson.augustin@yahoo.com>
2019-04-25 09:55:18 -0400

    changed modal id to class

--- docs/js/main.js
+++ docs/js/main.js
@@ -311,18 +311,22 @@ function sponsorTemplate(sponsor, levels) {
 "use strict";
 
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.getElementsByClassName('myModal');
 
 // Get the button that opens the modal
-var modalTrigger = document.getElementById("modalTrigger");
+var modalTrigger = document.getElementsByClassName("modalTrigger");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-modalTrigger.onclick = function () {
+// modalTrigger.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+modalTrigger.addEventListener("click", function () {
   modal.style.display = "block";
-};
+});
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,20 @@
 // Get the modal
-let modal = document.getElementById('myModal');
+let modal = document.getElementsByClassName('myModal');
 
 // Get the button that opens the modal
-let modalTrigger = document.getElementById("modalTrigger");
+let modalTrigger = document.getElementsByClassName("modalTrigger");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-modalTrigger.onclick = function() {
-  modal.style.display = "block";
-}
+// modalTrigger.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+modalTrigger.addEventListener("click", ()=>{
+    modal.style.display = "block";
+})
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
