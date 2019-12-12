1ffc7114e4b638d68c1d929c3834be7e74647342
axecopfire <sbraun990@gmail.com>
2019-12-12 16:55:07 -0500

    init migration to nuxt

---
+++ southern-dev/components/BottomFooter.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+footer.footerMain
+  div.clubhouse
+    a(target="_blank" rel="noreferrer noopener" href="https://theclubhou.se/")  Organized by theClubhou.se and friends
+    a(target="_blank" rel="noreferrer noopener" href="https://theclubhou.se/")
+      img(src="images/theclubhouse.png" alt="Blue circle surrounding a big C with theClubhouse written inside")
+  div.main
+    ul
+      li
+        ul#social
+          li
+            a(target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/SouthernDevConference")
+              i.fab.fa-facebook-square
+          li
+            a(target="_blank" rel="noreferrer noopener" href="https://twitter.com/sodevcon")
+              i.fab.fa-twitter
+          li
+            a(target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/company/sodevcon/about/")
+              i.fab.fa-linkedin
+      li
+        a(target="_blank" rel="noreferrer noopener" href="mailto:info@southerndev.co")
+          i.far.fa-envelope
+          |  info@southerndev.co
+      li
+        a(target="_blank" rel="noreferrer noopener" href="https://augustadevs.github.io/") ❤️ by Augusta Devs
+      li
+        a(target="_blank" rel="noreferrer noopener" href="https://github.com/theClubhouse-Augusta/southern-dev")
+          i.fab.fa-github
+          |  Our code
+</template>
---
+++ southern-dev/components/TopHeader.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  doctype html
+  html
+    //- include head.html
+    body
+      .hamburger
+          span.line
+          span.line
+          span.line
+      header#header-main
+        a(href="index")
+          //- img(src="images/logo.png").header-image
+        //- include ../components/navbar.pug
+</template>
