c061b53d44681fb1b81fc46ad28b1e291353c5b5
axecopfire <sbraun990@gmail.com>
2020-01-19 18:13:19 -0500

    speaker styling

---
+++ components/Speaker/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+    .speaker(@click="active = !active")
+        Modal(v-if="active" :speaker="speaker")
+        .speaker-img-container
+            img.speaker-img(:src="require(`~/assets${speaker.speakerImage}`)")
+        .speaker-name-container
+            h5.speaker-name {{speaker.name}}
+</template>
+<script>
+import Modal from './modal';
+export default {
+    components: {
+        Modal
+    },
+    data () {
+        return { 
+            active: false 
+        }
+    },
+    props: ["speaker"],
+    mounted () {
+        console.log("speaker", this.speaker)
+        console.log("index", this.index);
+    }
+} 
+</script>
+
+<style lang="sass" scoped>
+.speaker
+    position: relative
+    border-radius: 5px
+    box-shadow: $shadow1, $shadow3
+    overflow: hidden
+
+.speaker-img-container
+    width: 100%
+    height: 100%
+img
+    width: 100%
+h5
+    background-color: $primary-blue
+    position: absolute
+    bottom: 0
+    width: 100%
+</style>
---
+++ components/Speaker/modal.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+    section.modal
+        //- img.speaker-image(:src="require(`~/assets${speaker.speakerImage}`)")
+        //- h4.speaker-name {{speaker.name}}
+        
+        //- h1.talk-title {{speaker.talkTitle}}
+        //- Modal(v-if="active" :speaker="speaker")
+        //- h5.speaker-title {{speaker.speakerTitle}}
+        //- div.speaker-info
+        //- div.bio
+        //-     div {{speaker.bio}}
+</template>
+<script>
+export default {
+    data () {
+        return {
+            active: false
+        }
+    },
+    props: ["speaker"]
+}
+</script>
+
+<style lang="sass" scoped>
+.speaker-image
+    margin: 0 10px
+    border-radius: 100%
+    width: 250px
+    border: 1px solid $gray5
+    @media(min-width: 900px)
+        float: left
+h1
+  font-size: 4rem
+  background-color: $blue1
+  color: $blue7
+  font-weight: bold
+
+li
+  text-align: left
+  padding: 0 4em
+
+.row
+    display: flex
+    padding-bottom: 2em
+
+.talk-info
+    flex-basis: 80%
+</style>
